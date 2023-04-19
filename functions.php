bb8b48c51e343648bcb9637d6877a612c4e46a54
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-04-19 16:49:42 +0200

    redirect a password.php

---
+++ functions.php
@@ -0,0 +1,79 @@
+<?php
+
+$charrepeat = $_GET['same_chars'] ?? '';
+$charincludes = $_GET['characters'] ?? '';
+$pwlength = $_GET['pw_length'] ?? '';
+
+//session per pagina di output passowrd
+if ($pwlength >= 5 && $pwlength <= 32) {
+    session_start();
+    $_SESSION['charincludes'] = $charincludes;
+    $_SESSION['pwlength'] = $pwlength;
+    $_SESSION['norepeat'] = $charrepeat;
+    header('Location: ./password.php');
+}
+
+function getRandomPW($length, $includechar, $norepeat)
+{
+    //numeri e char speciali disponibili
+    $numbers = str_split('0123456789');
+    $specials = str_split('!@#$%^&*()');
+    $lowerletters = str_split('abcdefghijklmnopqrstuvwxyz');
+    $upperletters = str_split('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    $chars = [];
+
+
+    if (empty($includechar)) {
+        $chars = array_merge($numbers, $lowerletters, $upperletters, $specials);
+    } else {
+        //controllo per generazione caratteri disponibili in base alle checkbox
+        if (in_array("numbers", $includechar)) {
+            $chars = array_merge($chars, $numbers);
+        }
+
+        if (in_array("specialchars", $includechar)) {
+            $chars = array_merge($chars, $specials);
+        }
+
+        if (in_array("letters", $includechar)) {
+            $chars = array_merge($chars, $lowerletters, $upperletters);
+        }
+    }
+
+
+    $randompw = '';
+
+    if ($norepeat) {
+
+        // checkloop per numeri e caratteri speciali
+        if ($chars === $numbers && $length >= 10) {
+            return getDefaultPW($numbers);
+        }
+
+        if ($chars === $specials && $length >= 10) {
+            return getDefaultPW($specials);
+        }
+
+        //pw senza caratteri ripetuti
+        while (strlen($randompw) < $length) {
+            $temp = rand(0, count($chars) - 1);
+            if (!str_contains($randompw, $chars[$temp])) $randompw .= $chars[$temp];
+        }
+    } else {
+        //pw con caratteri ripetuti
+        while (strlen($randompw) < $length) {
+            $temp = rand(0, count($chars) - 1);
+            $randompw .= $chars[$temp];
+        }
+    }
+    return str_shuffle($randompw);
+}
+
+
+//genera pw di default per evitare il loop
+function getDefaultPW($arr)
+{
+    $randomdefault = '';
+    $randomdefault = implode("", $arr);
+    return str_shuffle($randomdefault);
+}
--- index.php
+++ index.php
@@ -25,34 +25,70 @@ require_once __DIR__ . '/functions.php';
 ?>
 
 
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- bootstrap -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
 
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>PW Generator</title>
 </head>
+
 <body>
-   
-    <h1 class="py-5 text-center">STRONG PASSWORD GENERATOR</h1>
+    <h1 class="py-5">STRONG PASSWORD GENERATOR</h1>
     <div class="container flex-column py-5">
         <div class="row d-flex flex-column justify-content-center">
             <form action="index.php" method="GET">
 
                 <div class="col">
 
+                    <label for="basic-url" class="form-label">Inserisci (un numero compreso tra 5 e 32) la lunghezza della password generata.</label>
                     <div class="input-group mb-3 w-50 m-auto">
-                        <label for="pw_length" style="padding: 10px;" >Decidi la lunghezza della tua password:</label>
-                        <input type="number" class="form-control text-center p-1" id="pw_length"  name="pw_length">
+                        <input type="number" class="form-control text-center" id="basic-url" name="pw_length" >
                     </div>
                 </div>
 
-                
+                <div class="col d-flex justify-content-center">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="same_chars" id="flexRadioDefault1" value="0" checked>
+                        <label class="form-check-label" for="flexRadioDefault1">
+                            Caratteri uguali ammessi
+                        </label>
+                    </div>
+                    <div class="form-check mx-4">
+                        <input class="form-check-input" type="radio" name="same_chars" id="flexRadioDefault2" value="1">
+                        <label class="form-check-label" for="flexRadioDefault2">
+                            NO caratteri uguali
+                        </label>
+                    </div>
+                </div>
 
-               
+                <div class="col d-flex my-3 flex-column">
+                    <p>Includi:</p>
+                    <div class="checkbox-form d-flex">
+                        <input type="checkbox" id="numbers" value="numbers" name="characters[]" checked>
+                        <label for="numbers" class="mx-3">Numeri</label>
+                    </div>
+
+                    <div class="checkbox-form d-flex">
+                        <input type="checkbox" id="letters" value="letters" name="characters[]" checked>
+                        <label for="numbers" class="mx-3">Lettere</label>
+                    </div>
+
+                    <div class="checkbox-form d-flex">
+                        <input type="checkbox" id="specialchars" value="specialchars" name="characters[]" checked>
+                        <label for="numbers" class="mx-3">Caratteri speciali</label>
+                    </div>
+
+                </div>
+
                 <div class="col d-flex justify-content-center py-3">
                     <button class="btn btn-info" type="submit">GENERA</button>
                 </div>
@@ -60,5 +96,8 @@ require_once __DIR__ . '/functions.php';
             </form>
         </div>
 
+        
+    </div>
 </body>
+
 </html>
